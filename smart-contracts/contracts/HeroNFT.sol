234f794f4e08c4f6c79b2ffda76b96146ab558f6
0x332 <kinspotting@protonmail.com>
2019-07-09 16:19:40 +0300

    wip: add heronft

---
+++ smart-contracts/contracts/HeroNFT.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.0;
+
+// import "./common/Ownable.sol";
+// import "./MetadataStore.sol";
+import 'node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+
+/**
+* @title HeroToken
+* @author Arseny Kin
+* @notice Contract for ERC721 Hero token
+*/
+contract HeroToken is ERC721Full/*, Ownable, MetadataStore*/ {
+
+    address public blocklords;
+
+    function setBlocklordsAddress(address _blocklords) public onlyOwner {
+        blocklords = _blocklords;
+    }
+  
+	constructor() ERC721Full("HeroToken", "BLT") public { }
+
+    /**
+    * @dev calculates the next token ID based on totalSupply
+    * @return uint256 for the next token ID
+    */
+    function _getNextTokenId() private view returns (uint256) {
+        return totalSupply().add(1); 
+    }
+
+    /**
+    * @dev Mints a token to an address 
+    * @param _to address of the future owner of the token
+    * @return uint256 for the token ID
+    */
+ 
+	function mintTo(address _to) public returns(uint256){
+        require(msg.sender == blocklords, // TODO: add signature check
+            "Only blocklords contract can initiate this transaction");
+
+		uint256 newTokenId = _getNextTokenId();
+		_mint(_to, newTokenId);
+		return newTokenId;
+	}
+
+}
