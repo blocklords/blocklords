7847bcdc7bc078467d6a6598847ee77a38001bc2
0x332 <kinspotting@protonmail.com>
2019-07-10 18:06:02 +0300

    fix: addHero

--- smart-contracts/contracts/HeroFactory.sol
+++ smart-contracts/contracts/HeroFactory.sol
@@ -17,4 +17,16 @@ contract HeroFactory {
 
     mapping (uint => Hero) heroes;
 
+    function addHero(uint[] memory _heroStats/*, uint[] _heroItems*/) public payable returns(bool) {
+        
+        _id = heroToken.mintTo(msg.sender); // NOTE: temporary solution!!
+        heroes[_id] = Hero( _heroStats[0], _heroStats[1],  _heroStats[2], _heroStats[3], _heroStats[4], block.number);
+
+        return true;
+    }
+
+    function getHero(uint id) public view returns(uint, uint, uint, uint, uint, uint){
+        return (heroes[id].LEADERSHIP, heroes[id].INTELLIGENCE, heroes[id].STRENGTH, heroes[id].SPEED, heroes[id].DEFENSE, heroes[id].CREATED_TIME);
+    }
+
 }
