d61be3b94561f63479da00485aba6baa50c87ca9
0x332 <kinspotting@protonmail.com>
2019-07-22 12:38:02 +0500

    add: itemFactory test

---
+++ smart-contracts/test/itemFactory_spec.js
@@ -0,0 +1,27 @@
+const HeroToken = require('Embark/contracts/HeroToken');
+const ItemFactory = require('Embark/contracts/ItemFactory');
+
+let accounts;
+
+config({
+  contracts: {
+    "HeroToken": {
+      args: ["Hero", "HERO"]
+    },
+    "ItemFactory": {
+      args: ["$HeroToken"]
+    }
+  }
+}, (_err, web3_accounts) => {
+  accounts = web3_accounts
+});
+
+// contract("HeroFactory", function () {
+
+//   it("should create a hero", async function () {
+//     await HeroToken.methods.addMinter(HeroFactory.address).send();
+//     await HeroFactory.methods.addHero().send();
+//   });
+
+// })
+;
