2b763c3e76744c886a7ba66e1a903fd06cd0e579
0x332 <kinspotting@protonmail.com>
2019-07-22 16:24:31 +0500

    fix: tests

--- smart-contracts/test/heroFactory_spec.js
+++ smart-contracts/test/heroFactory_spec.js
@@ -26,7 +26,7 @@ contract("HeroFactory", function () {
 
   it("should create a hero", async function () {
 
-    let hero = await HeroFactory.methods.createHero(HeroFactory.address, 1, 2, 3, 4, 5, 6, 7, 8).send();
+    let hero = await HeroFactory.methods.createHero(HeroFactory.address, 1, 2, 3, 4, 5, 6, 7, 0).send();
     let id = hero.events.HeroCreated.returnValues.id;
 
     assert.strictEqual('1', id);
@@ -36,7 +36,7 @@ contract("HeroFactory", function () {
   it("should not allow to create a hero", async function () {
 
     try {
-      let hero = await HeroFactory.methods.createHero(HeroFactory.address, 1, 2, 3, 4, 5, 6, 7, 8).send({from: accounts[2]});
+      let hero = await HeroFactory.methods.createHero(HeroFactory.address, 1, 2, 3, 4, 5, 6, 7, 0).send({from: accounts[2]});
       assert.fail('should have reverted before');
     }
     catch (error) {
@@ -48,17 +48,17 @@ contract("HeroFactory", function () {
   it("should get hero info", async function () {
 
     let heroInfo = await HeroFactory.methods.getHeroInfo('1').call();
-    assert.strictEqual('1', id);   
+    assert.strictEqual('1', heroInfo[0]);   
 
   });
 
   it("should make hero older", async function () {
 
-    let heroInfo = await HeroFactory.methods.getHeroInfo('1').call();
-    assert.strictEqual('1', id);   
-    
-  });
+    await HeroFactory.methods.makeOlder('1').send();
+    let hero = await HeroFactory.methods.getHeroInfo('1').call();
 
+    assert.strictEqual('1', hero[3]);   
+  });
 
 })
 ;
